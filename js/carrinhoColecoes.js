9a3c00155c85c1db20996b1b51cb24883484cd5a
simonea30 <simonealmeida2030@gmail.com>
2022-11-27 15:23:41 -0300

    Novos ajustes coleções

---
+++ js/carrinhoColecoes.js
@@ -0,0 +1,48 @@
+// Definir variáveis universais                                                                                                               
+var valorTotal = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+var valorProduto = [0, 240, 240, 240, 240, 240, 240, 240, 270, 270, 270, 270, 380, 380, 380, 380, 170, 170, 170, 170, 170, 200, 150, 150, 150, 150, 150, 150, 200, 200, 200, 350, 350, 310, 310, 310];                                                                           
+var qtd = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+// Aumentar a quantidade de itens de um produto
+function adicionarItem(item) {
+	var quantidade = document.getElementById('quantidade' + item);
+	var total = document.getElementById('total' + item);
+	qtd[item] += 1;
+	valorTotal[item] = valorProduto[item] * qtd[item];
+	quantidade.innerHTML = qtd[item];
+	total.innerHTML = valorTotal[item].toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});;
+	console.log(quantidade);
+	valorCompra();
+}
+
+// Diminuir a quantidade de itens de um produto
+function removerItem(item) {
+	if (qtd[item] > 0) {
+		qtd[item] -= 1;
+		var quantidade = document.getElementById('quantidade' + item);
+		var total = document.getElementById('total' + item);
+		quantidade.innerHTML = qtd[item];
+		valorTotal[item] = valorProduto[item] * qtd[item];
+		total.innerHTML = valorTotal[item];
+		valorCompra();
+	}
+}
+
+// Calcular o valor total da compra
+function valorCompra() {
+	var valorTotalCompra = document.getElementById('valorTotalCompra');
+	var valor = 0;;
+	for (var i = 0; i < valorTotal.length; i++) {
+		valor += valorTotal[i];
+	}
+	valorTotalCompra.innerHTML = valor.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
+	console.log(valorTotal.length);
+}
+
+
+// Finalizar a compra e zerar os valores
+function finalizarCompra() {
+	var nome = document.getElementById('nome').innerHTML;
+	alert('Obrigada pela compra, ' + nome + '!');
+	document.location.reload(true);
+}
